81e78e403076d8174e6e893fde451b15c2b2c52d
maopl <451052682@qq.com>
2019-08-01 13:21:29 +0800

    Add files via upload

--- metaheuristics/HypE.c
+++ metaheuristics/HypE.c
@@ -14,6 +14,137 @@
 
 
 
+void rearrangeIndicesByColumn(double *mat, int rows, int columns, int col,
+                              int *ind )
+/**
+ * Internal function used by hypeExact
+ */
+{
+#define  MAX_LEVELS  300
+    int  beg[MAX_LEVELS], end[MAX_LEVELS], i = 0, L, R, swap;
+    double pref, pind;
+    double ref[rows];
+    for( i = 0; i < rows; i++ ) {
+        ref[i] = mat[ col + ind[i]*columns ];
+    }
+    i = 0;
+
+    beg[0] = 0; end[0] = rows;
+    while ( i >= 0 ) {
+        L = beg[i]; R = end[i]-1;
+        if( L < R ) {
+            pref = ref[ L ];
+            pind = ind[ L ];
+            while( L < R ) {
+                while( ref[ R ] >= pref && L < R )
+                    R--;
+                if( L < R ) {
+                    ref[ L ] = ref[ R ];
+                    ind[ L++] = ind[R];
+                }
+                while( ref[L] <= pref && L < R )
+                    L++;
+                if( L < R) {
+                    ref[ R ] = ref[ L ];
+                    ind[ R--] = ind[L];
+                }
+            }
+            ref[ L ] = pref; ind[L] = pind;
+            beg[i+1] = L+1; end[i+1] = end[i];
+            end[i++] = L;
+            if( end[i] - beg[i] > end[i-1] - beg[i-1] ) {
+                swap = beg[i]; beg[i] = beg[i-1]; beg[i-1] = swap;
+                swap = end[i]; end[i] = end[i-1]; end[i-1] = swap;
+            }
+        }
+        else {
+            i--;
+        }
+    }
+}
+
+void hypeExactRecursive( double* input_p, int pnts, int dim, int nrOfPnts,
+                         int actDim, double* bounds, int* input_pvec, double* fitness,
+                         double* rho, int param_k )
+/**
+ * Internal function used by hypeExact
+ */
+{
+    int i, j;
+    double extrusion;
+    int pvec[pnts];
+    double p[pnts*dim];
+    for( i = 0; i < pnts; i++ ) {
+        fitness[i] = 0;
+        pvec[i] = input_pvec[i];
+    }
+    for( i = 0; i < pnts*dim; i++ )
+        p[i] = input_p[i];
+
+    rearrangeIndicesByColumn( p, nrOfPnts, dim, actDim, pvec );
+
+    for( i = 0; i < nrOfPnts; i++ )
+    {
+        if( i < nrOfPnts - 1 )
+            extrusion = p[ (pvec[i+1])*dim + actDim ] - p[ pvec[i]*dim + actDim ];
+        else
+            extrusion = bounds[actDim] - p[ pvec[i]*dim + actDim ];
+
+        if( actDim == 0 ) {
+            if( i+1 <= param_k )
+                for( j = 0; j <= i; j++ ) {
+                    fitness[ pvec[j] ] = fitness[ pvec[j] ]
+                                         + extrusion*rho[ i+1 ];
+                }
+        }
+        else if( extrusion > 0 ) {
+            double tmpfit[ pnts ];
+            hypeExactRecursive( p, pnts, dim, i+1, actDim-1, bounds, pvec,
+                                tmpfit, rho, param_k );
+            for( j = 0; j < pnts; j++ )
+                fitness[j] += extrusion*tmpfit[j];
+        }
+    }
+}
+
+
+void hypeExact( Fitness_info_t * fitnessInfo, int param_k, double* rho, SMRT_individual *pop_table, int pop_num)
+/**
+ * Calculating the hypeIndicator
+ * \f[ \sum_{i=1}^k \left( \prod_{j=1}^{i-1} \frac{k-j}{|P|-j} \right) \frac{ Leb( H_i(a) ) }{ i } \f]
+ */
+{
+    int i, j;
+    double boundsVec[ g_algorithm_entity.algorithm_para.objective_number ];
+    double *fitness;
+    double p[pop_num * g_algorithm_entity.algorithm_para.objective_number];
+    int indices[ pop_num ];
+
+    fitness = (double *)malloc(sizeof(double) * pop_num);
+
+    for (i = 0; i < pop_num; ++i)
+    {
+        for (j = 0; j < g_algorithm_entity.algorithm_para.objective_number; ++j)
+        {
+            p[i * g_algorithm_entity.algorithm_para.objective_number + j] = pop_table[i].obj[j];
+        }
+    }
+
+    for( i = 0; i < g_algorithm_entity.algorithm_para.objective_number; i++ )
+        boundsVec[i] = g_algorithm_entity.variable_higher_bound[i];
+    for( i = 0; i < pop_num; i++  )
+        indices[i] = i;
+
+    /** Recursively calculate the indicator values */
+    hypeExactRecursive( p, pop_num, g_algorithm_entity.algorithm_para.objective_number, pop_num, g_algorithm_entity.algorithm_para.objective_number-1, boundsVec,
+                        indices, fitness, rho, param_k );
+
+    for (i = 0; i < pop_num; ++i)
+    {
+        fitnessInfo[i].fitness = fitness[i];
+    }
+    free(fitness);
+}
 
 /* HypE sampling procedure for Hypervolume approximation */
 double hypeSampling (Fitness_info_t *fitnessInfo, int nrOfSamples, int param_k, double *rho, SMRT_individual *pop_table, int pop_num)
@@ -111,9 +242,14 @@ void HypE_hypeIndicator(Fitness_info_t *fitnessInfo, int nrOfSamples, int param_
         fitnessInfo[i].fitness = 0.0;
 
     if( nrOfSamples < 0 )
-        ;//hypeExact( val, popsize, lowerbound, upperbound, param_k, points, rho);
+        hypeExact( fitnessInfo, param_k, rho, pop_table, pop_num);
     else
         hypeSampling(fitnessInfo, nrOfSamples, param_k, rho, pop_table, pop_num);
+
+    for (i = 0; i < pop_num; i++)
+    {
+        printf("index:%d, fit:%f\n", i, fitnessInfo[i].fitness);
+    }
 
 }
 
@@ -134,7 +270,7 @@ static void HypE_set_fitness(SMRT_individual *pop_table, int pop_num, int param_
 
     if (g_algorithm_entity.algorithm_para.objective_number <= 3)
     {
-        HypE_hypeIndicator(fitnessInfo, 10000, param_k, pop_table, pop_num);
+        HypE_hypeIndicator(fitnessInfo, -1, param_k, pop_table, pop_num);
     }
     else
     {
